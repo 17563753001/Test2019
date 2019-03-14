8d24dc0bcedd4d82289274e4217d02e2c68fefac
毅飞 滕 <1289754575@qq.com>
2019-03-14 18:35:01 +0800

    啊哈哈

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -13,6 +13,8 @@ namespace ConsoleApp1
             Console.WriteLine("我是爱人");
             //Console.WriteLine("我也是爱人");
             Console.WriteLine("这是我的信息");
+            Console.WriteLine("这是的信息");
+          
         }
     }
 }
